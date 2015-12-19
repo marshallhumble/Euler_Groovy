0703a189c80cf3363ce68a139fff6b151cf3db56
Marshall Humble <marshallhumble@users.noreply.github.com>
2015-12-19 06:58:11 -0600

    Update 1.groovy

--- 1.groovy
+++ 1.groovy
@@ -1,7 +1,7 @@
 /*
-//`it` is an implicit parameter corresponding to the current element, while `i` is the index
-//So we can use a range, specify an or statement then sum & print the total.
-*/
+ *`it` is an implicit parameter corresponding to the current element, while `i` is the index
+ *So we can use a range, specify an or statement then sum & print the total.
+ */
 
 
 println((1..<1000).findAll({ it % 3 == 0 || it % 5 == 0 }).sum())
