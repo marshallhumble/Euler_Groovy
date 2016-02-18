ab9e72590695b061c8e31321c54b822c74428454
Marshall Humble (C) <marshall_humble@apple.com>
2016-02-18 14:49:19 -0600

    Added timing

--- 13.py
+++ 13.py
@@ -1,6 +1,17 @@
+#!/usr/bin/env python
+
+import time
+
+'''
+Work out the first ten digits of the sum of the following one-hundred 50-digit numbers. (Numbers in text file)
+'''
+
+startTime = time.time()
 
 filename = '13_numbers_to_sum.txt'
 
 total = sum([int(s.strip()) for s in open(filename).readlines()])
 
 print(str(total)[:10])
+
+print(time.time() - startTime)
