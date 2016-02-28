29e3b8609a6f28dff24beb983c1598e88ab19747
Marshall Humble <humblejm@gmail.com>
2016-02-28 15:00:48 -0600

    added pytest for CodeIO

--- CheckIO/First_Island/1_solve_median.py
+++ CheckIO/First_Island/1_solve_median.py
@@ -1,5 +1,19 @@
 #!/usr/bin/env python
 
+"""
+A median is a numerical value separating the upper half of a sorted array of numbers from the lower half. In a list
+where there are an odd number of entities, the median is the number found in the middle of the array. If the array
+contains an even number of entities, then there is no single middle value, instead the median becomes the average of
+the two numbers found in the middle. For this mission, you are given a non-empty array of natural numbers (X). With it,
+you must separate the upper half of the numbers from the lower half and find the median.
+
+Input: An array as a list of integers.
+
+Output: The median as a float or an integer.
+
+"""
+
+
 def checkio(data):
     sorted_data = sorted(data)
     data_len = len(data)
@@ -11,8 +25,7 @@ def checkio(data):
         return (sorted_data[index] + sorted_data[index + 1]) / 2.0
 
 
-# These "asserts" using only for self-checking and not necessary for auto-testing
-if __name__ == '__main__':
+def test_function():
     assert checkio([1, 2, 3, 4, 5]) == 3, "Sorted list"
     assert checkio([3, 1, 2, 5, 3]) == 3, "Not sorted list"
     assert checkio([1, 300, 2, 200, 1]) == 2, "It's not an average"
@@ -20,3 +33,7 @@ if __name__ == '__main__':
     print("Start the long test")
     assert checkio(list(range(1000000))) == 499999.5, "Long."
     print("The local tests are done.")
+
+
+if __name__ == '__main__':
+    test_function()
--- CheckIO/First_Island/2_House_Passwords.py
+++ CheckIO/First_Island/2_House_Passwords.py
@@ -38,14 +38,18 @@ def checkio(data):
     else:
         return False
 
-if __name__ == '__main__':
-    # These "asserts" using only for self-checking and not necessary for auto-testing
+
+def test_function():
     assert checkio('A1213pokl') == False, "1st example"
     assert checkio('bAse730onE4') == True, "2nd example"
     assert checkio('asasasasasasasaas') == False, "3rd example"
     assert checkio('QWERTYqwerty') == False, "4th example"
     assert checkio('123456123456') == False, "5th example"
     assert checkio('QwErTy911poqqqq') == True, "6th example"
+
+
+if __name__ == '__main__':
+    test_function()
 
 """
 Regex Solution:
