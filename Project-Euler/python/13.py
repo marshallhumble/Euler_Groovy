484c6b0182e00515fa776c874e2b823d5c120e6c
Marshall Humble <humblejm@gmail.com>
2016-02-28 17:28:24 -0600

    Added utf lines for py2.7, commented out 13 on Euler b/c of file load error

--- Project-Euler/python/13.py
+++ Project-Euler/python/13.py
@@ -4,23 +4,25 @@
 Work out the first ten digits of the sum of the following one-hundred 50-digit numbers. (Numbers in text file)
 """
 
-import time
+# import time
 
-filename = '13_numbers_to_sum.txt'
-
-
-def get_sol():
-    total = sum([int(s.strip()) for s in open(filename).readlines()])
-    return int(str(total)[:10])
+# filename = '13_numbers_to_sum.txt'
 
 
-start = time.time()
-
-print(get_sol())
-
-elapsed = (time.time() - start)
-print("\nThis code took: {} seconds".format(str(elapsed)))
+# def get_sol():
+#    total = sum([int(s.strip()) for s in open(filename).readlines()])
+#    return int(str(total)[:10])
 
 
-#def test_function():
-#    assert get_sol() == 5537376230
+# start = time.time()
+
+# print(get_sol())
+
+# elapsed = (time.time() - start)
+# print("\nThis code took: {} seconds".format(str(elapsed)))
+
+
+
+
+# def test_function():
+#   assert get_sol() == 5537376230
