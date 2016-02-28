6af78318dd786ea67ce3ebdf602b93df5e95290c
Marshall Humble <humblejm@gmail.com>
2016-02-28 13:47:43 -0600

    Added CheckIO

---
+++ CheckIO/First_Island/1_solve_median.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+def checkio(data):
+    sorted_data = sorted(data)
+    data_len = len(data)
+    index = (data_len - 1) // 2
+
+    if data_len % 2:
+        return sorted_data[index]
+    else:
+        return (sorted_data[index] + sorted_data[index + 1]) / 2.0
+
+
+# These "asserts" using only for self-checking and not necessary for auto-testing
+if __name__ == '__main__':
+    assert checkio([1, 2, 3, 4, 5]) == 3, "Sorted list"
+    assert checkio([3, 1, 2, 5, 3]) == 3, "Not sorted list"
+    assert checkio([1, 300, 2, 200, 1]) == 2, "It's not an average"
+    assert checkio([3, 6, 20, 99, 10, 15]) == 12.5, "Even length"
+    print("Start the long test")
+    assert checkio(list(range(1000000))) == 499999.5, "Long."
+    print("The local tests are done.")
---
+++ CheckIO/First_Island/2_House_Passwords.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+
+import re
+
+"""
+Stephan and Sophia forget about security and use simple passwords for everything. Help Nikola develop a password
+security check module. The password will be considered strong enough if its length is greater than or equal to
+10 symbols, it has at least one digit, as well as containing one uppercase letter and one lowercase letter in it. The
+password contains only ASCII latin letters or digits.
+Input: A password as a string (Unicode for python 2.7).
+Output: Is the password safe or not as a boolean or any data type that can be converted and processed as a boolean.
+In the results you will see the converted results.
+
+Precondition:
+re.match("[a-zA-Z0-9]+", password)
+0 < len(password) ≤ 64
+"""
+
+
+def checkio(data):
+    upper_let = 0
+    digit_let = 0
+    let_low = 0
+
+    if len(data) < 10:
+        return False
+
+    for let in data:
+        if let.isupper():
+            upper_let += 1
+        if let.isdigit():
+            digit_let += 1
+        if let.islower():
+            let_low += 1
+
+    if upper_let >= 1 and digit_let >= 1 and let_low >= 1:
+        return True
+    else:
+        return False
+
+if __name__ == '__main__':
+    # These "asserts" using only for self-checking and not necessary for auto-testing
+    assert checkio('A1213pokl') == False, "1st example"
+    assert checkio('bAse730onE4') == True, "2nd example"
+    assert checkio('asasasasasasasaas') == False, "3rd example"
+    assert checkio('QWERTYqwerty') == False, "4th example"
+    assert checkio('123456123456') == False, "5th example"
+    assert checkio('QwErTy911poqqqq') == True, "6th example"
+
+"""
+Regex Solution:
+
+import re
+​
+def checkio(data):
+    return len(data)>9 and len(data) <= 64 and re.search("[a-z]+", data) and re.search("[A-Z]+", data) and re.search("[0-9]+", data) and re.search("[a-zA-Z0-9]+", data)
+"""
