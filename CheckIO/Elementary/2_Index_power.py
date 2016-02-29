9c9432d8640c9298ba886538e174195158ecc07a
Marshall Humble <humblejm@gmail.com>
2016-02-28 18:31:42 -0600

    added utf line for py2.7

--- CheckIO/Elementary/2_Index_power.py
+++ CheckIO/Elementary/2_Index_power.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 """
 You are given an array with positive numbers and a number N. You should find the N-th power of the element in the array with the index N. If N is outside of the array, then return -1. Don't forget that the first element has the index 0.
