7bb559e31e3980f58159efdab30c4d1b31501890
Marshall Humble <humblejm@gmail.com>
2016-02-28 18:27:53 -0600

    Added Index Power, added in docstring for fizz buzz

--- CheckIO/Elementary/1_FizzBuzz.py
+++ CheckIO/Elementary/1_FizzBuzz.py
@@ -1,3 +1,18 @@
+#!/usr/bin/env python
+
+"""
+"Fizz buzz" is a word game we will use to teach the robots about division. Let's learn computers.
+
+You should write a function that will receive a positive integer and return:
+"Fizz Buzz" if the number is divisible by 3 and by 5;
+"Fizz" if the number is divisible by 3;
+"Buzz" if the number is divisible by 5;
+The number as a string for other cases.
+Input: A number as an integer.
+
+Output: The answer as a string.
+"""
+
 def checkio(number):
     if number % 3 == 0 and number % 5 == 0:
         return "Fizz Buzz"
