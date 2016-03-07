98494eb2f06c61a5c23afe65a5a5ee2e6241f2a4
Marshall Humble <humblejm@gmail.com>
2016-03-06 21:14:00 -0600

    Add Solve Digit Multiplication

---
+++ CheckIO/Elementary/7_Solve_Digit_Multiplication.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+"""
+You are given a positive integer. Your function should calculate the product of the digits excluding any zeroes.
+
+For example: The number given is 123405. The result will be 1*2*3*4*5=120 (don't forget to exclude zeroes).
+
+Input: A positive integer.
+
+Output: The product of the digits as an integer.
+
+
+"""
+
+from functools import reduce
+
+checkio = lambda number: reduce(lambda res, x: res*x, [int(num) for num in str(number) if int(num)], 1)
+
+# These "asserts" using only for self-checking and not necessary for auto-testing
+if __name__ == '__main__':
+    assert checkio(123405) == 120
+    assert checkio(999) == 729
+    assert checkio(1000) == 1
+    assert checkio(1111) == 1
