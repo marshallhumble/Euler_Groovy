e91efeac1b0cd777a80010070509c47b5ef16423
Marshall Humble <humblejm@gmail.com>
2016-03-06 20:58:45 -0600

    Added Non Unique Elements

---
+++ CheckIO/First_Island/4_Non_Unique_Elements.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+
+"""
+You are given a non-empty list of integers (X). For this task, you should return a list consisting of only the non-unique elements in this list. To do so you will need to remove all unique elements (elements which are contained in a given list only once). When solving this task, do not change the order of the list. Example: [1, 2, 3, 1, 3] 1 and 3 non-unique elements and result will be [1, 3, 1, 3].
+
+non-unique-elements
+
+Input: A list of integers.
+
+Output: The list of integers.
+
+Example:
+
+checkio([1, 2, 3, 1, 3]) == [1, 3, 1, 3]
+checkio([1, 2, 3, 4, 5]) == []
+checkio([5, 5, 5, 5, 5]) == [5, 5, 5, 5, 5]
+checkio([10, 9, 10, 10, 9, 8]) == [10, 9, 10, 10, 9]
+
+"""
+
+
+def checkio(data):
+    return [x for x in data if data.count(x) > 1]
+
+
+def test_function():
+    assert isinstance(checkio([1]), list), "The result must be a list"
+    assert checkio([1, 2, 3, 1, 3]) == [1, 3, 1, 3], "1st example"
+    assert checkio([1, 2, 3, 4, 5]) == [], "2nd example"
+    assert checkio([5, 5, 5, 5, 5]) == [5, 5, 5, 5, 5], "3rd example"
+    assert checkio([10, 9, 10, 10, 9, 8]) == [10, 9, 10, 10, 9], "4th example"
+
+
+if __name__ == "__main__":
+    test_function()
