84e6bd6883051ff38a619592e4bfd1dda5fd0789
Marshall Humble (C) <marshall_humble@apple.com>
2016-04-06 16:51:52 -0500

    Added 2 more

---
+++ CheckIO/Home/6_Most_Wanted_Letter.py
@@ -0,0 +1,18 @@
+import collections
+import re
+
+def checkio(text):
+    counter = collections.Counter(filter(str.islower, map(str.lower, text)))
+    return sorted(filter(lambda letter: counter[letter] == counter.most_common(1)[0][1], counter))[0]
+
+if __name__ == '__main__':
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert checkio("Hello World!") == "l", "Hello test"
+    assert checkio("How do you do?") == "o", "O is most wanted"
+    assert checkio("One") == "e", "All letter only once."
+    assert checkio("Oops!") == "o", "Don't forget about lower case."
+    assert checkio("AAaooo!!!!") == "a", "Only letters."
+    assert checkio("abe") == "a", "The First."
+    print("Start the long test")
+    assert checkio("a" * 9000 + "b" * 1000) == "a", "Long."
+    print("The local tests are done.")
