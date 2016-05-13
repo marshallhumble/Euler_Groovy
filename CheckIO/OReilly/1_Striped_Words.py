0a2e56cc9ed684048fcd3e02b778221a1f7a53c8
Marshall Humble <humblejm@gmail.com>
2016-05-13 10:21:50 -0500

    Reformatted for pytest functionality

--- CheckIO/OReilly/1_Striped_Words.py
+++ CheckIO/OReilly/1_Striped_Words.py
@@ -66,8 +66,12 @@ def checkio(text):
 
 
 # These "asserts" using only for self-checking and not necessary for auto-testing
-if __name__ == '__main__':
+def test_function():
     assert checkio("My name is ...") == 3, "All words are striped"
     assert checkio("Hello world") == 0, "No one"
     assert checkio("A quantity of striped words.") == 1, "Only of"
     assert checkio("Dog,cat,mouse,bird.Human.") == 3, "Dog, cat and human"
+
+
+if __name__ == '__main__':
+    test_function()
