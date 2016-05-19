964ec62440c0f9f4666176f39b6b973c313f3c1c
Marshall Humble <humblejm@gmail.com>
2016-05-19 13:44:56 -0500

    added in function for pytest

--- CheckIO/Elementary/13_Days_Between.py
+++ CheckIO/Elementary/13_Days_Between.py
@@ -11,8 +11,12 @@ def days_diff(date1, date2):
     return days
 
 
-if __name__ == '__main__':
+def test_fucntion():
     # These "asserts" using only for self-checking and not necessary for auto-testing
     assert days_diff((1982, 4, 19), (1982, 4, 22)) == 3
     assert days_diff((2014, 1, 1), (2014, 8, 27)) == 238
     assert days_diff((2014, 8, 27), (2014, 1, 1)) == 238
+
+
+if __name__ == '__main__':
+    test_fucntion()
