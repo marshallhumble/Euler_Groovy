46f57b9d8630f1ac01266698c03fa7517e791885
Marshall Humble <humblejm@gmail.com>
2016-05-19 13:35:52 -0500

    Solved Elementary\Days Between

--- CheckIO/Elementary/13_Days_Between.py
+++ CheckIO/Elementary/13_Days_Between.py
@@ -1,19 +1,18 @@
 import datetime
-from datetime import date
+
 
 def days_diff(date1, date2):
     """
         Find absolute diff in days between dates
     """
-    d1 = date.strftime(date1, '%Y, %b, %d')
-    d2 = date.strftime(date2, '%Y, %m, %d')
-    d3 = d1 - d2
-    print(d3)
-    print('f')
+    days = (datetime.datetime(*date2) - datetime.datetime(*date1)).days
+    if days < 0:
+        return -days
+    return days
+
 
 if __name__ == '__main__':
     # These "asserts" using only for self-checking and not necessary for auto-testing
-    # assert days_diff((1982, 4, 19), (1982, 4, 22)) == 3
-    # assert days_diff((2014, 1, 1), (2014, 8, 27)) == 238
-    # assert days_diff((2014, 8, 27), (2014, 1, 1)) == 238
-    days_diff((1982, 4, 19), (1982, 4, 22))
+    assert days_diff((1982, 4, 19), (1982, 4, 22)) == 3
+    assert days_diff((2014, 1, 1), (2014, 8, 27)) == 238
+    assert days_diff((2014, 8, 27), (2014, 1, 1)) == 238
