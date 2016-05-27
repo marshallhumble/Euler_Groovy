2e4d109c60d2dad657a077cbd466e52037bf4eb3
Marshall Humble <humblejm@gmail.com>
2016-05-27 15:01:41 -0500

    Add 2 checks

--- CheckIO/Elementary/15_Common_Words.py
+++ CheckIO/Elementary/15_Common_Words.py
@@ -23,7 +23,10 @@ def checkio(first, second):
 
 
 # These "asserts" using only for self-checking and not necessary for auto-testing
-if __name__ == '__main__':
+def test_function():
     assert checkio("hello,world", "hello,earth") == "hello", "Hello"
     assert checkio("one,two,three", "four,five,six") == "", "Too different"
     assert checkio("one,two,three", "four,five,one,two,six,three") == "one,three,two", "1 2 3"
+
+if __name__ == '__main__':
+    test_function()
