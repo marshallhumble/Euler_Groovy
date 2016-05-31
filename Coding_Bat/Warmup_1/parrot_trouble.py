28010c3d9cca68c6f23a1ec0b631be80a3b06722
Marshall Humble <humblejm@gmail.com>
2016-05-31 15:54:50 -0500

    Created folder completed 5 warm ups

---
+++ Coding_Bat/Warmup_1/parrot_trouble.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+"""
+We have a loud talking parrot. The "hour" parameter is the current hour time in the range 0..23. We are in trouble if
+the parrot is talking and the hour is before 7 or after 20. Return True if we are in trouble.
+
+parrot_trouble(True, 6) → True
+parrot_trouble(True, 7) → False
+parrot_trouble(False, 6) → False
+"""
+
+
+def parrot_trouble(talking, hour):
+    if talking and (hour < 7 or hour > 20):
+        return True
+    else:
+        return False
+
+
+def test_function():
+    assert parrot_trouble(True, 6) == True
+    assert parrot_trouble(True, 7) == False
+    assert parrot_trouble(False, 6) == False
+    assert parrot_trouble(True, 21) == True
+    assert parrot_trouble(False, 21) == False
+    assert parrot_trouble(False, 20) == False
+    assert parrot_trouble(True, 23) == True
+    assert parrot_trouble(False, 23) == False
+    assert parrot_trouble(True, 20) == False
+    assert parrot_trouble(False, 12) == False
+
+
+if __name__ == '__main__':
+    test_function()
---
+++ Coding_Bat/Warmup_1/sum_double.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+"""
+Given two int values, return their sum. Unless the two values are the same, then return double their sum.
+
+sum_double(1, 2) → 3
+sum_double(3, 2) → 5
+sum_double(2, 2) → 8
+"""
+
+
+def sum_double(a, b):
+    if a == b:
+        return 2 * (a + b)
+    else:
+        return a + b
+
+
+def test_function():
+    assert sum_double(1, 2) == 3
+    assert sum_double(3, 2) == 5
+    assert sum_double(2, 2) == 8
+    assert sum_double(-1, 0) == -1
+    assert sum_double(3, 3) == 12
+    assert sum_double(0, 0) == 0
+    assert sum_double(0, 1) == 1
+    assert sum_double(3, 4) == 7
+
+
+if __name__ == '__main__':
+    test_function()
