264657ba49cdd97fcf1e123a25cf28a1e6fd0fea
Marshall Humble <humblejm@gmail.com>
2016-06-01 07:57:08 -0500

    Fixed encoding for 2.7 build

--- Coding_Bat/Warmup_1/sum_double.py
+++ Coding_Bat/Warmup_1/sum_double.py
@@ -3,9 +3,9 @@
 """
 Given two int values, return their sum. Unless the two values are the same, then return double their sum.
 
-sum_double(1, 2) → 3
-sum_double(3, 2) → 5
-sum_double(2, 2) → 8
+sum_double(1, 2) == 3
+sum_double(3, 2) == 5
+sum_double(2, 2) == 8
 """
 
 
