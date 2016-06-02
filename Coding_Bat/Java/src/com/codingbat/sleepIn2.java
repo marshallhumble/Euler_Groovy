e17b304de2fbb8fdcd8d7792ad2b24b99f74324a
Marshall Humble <humblejm@gmail.com>
2016-06-02 16:50:12 -0500

    Added first 3 java solutions

---
+++ Coding_Bat/Java/src/com/codingbat/sleepIn2.java
@@ -0,0 +1,11 @@
+package com.codingbat;
+
+/**
+ * Created by marshallhumble on 6/2/16.
+ */
+public class sleepIn2 {
+    public boolean sleepIn(boolean weekday, boolean vacation) {
+        return !(weekday && !vacation);
+    }
+}
+
---
+++ Coding_Bat/Java/src/com/codingbat/sumDouble.java
@@ -0,0 +1,11 @@
+package com.codingbat;
+
+/**
+ * Created by marshallhumble on 6/2/16.
+ */
+public class sumDouble {
+    public int sumDouble(int a, int b) {
+        return a == b ? 2 * (a + b) : a + b;
+    }
+
+}
